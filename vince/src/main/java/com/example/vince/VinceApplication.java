e84332bc39db03421b2cd31f425928326875a240
VincentP-EFREI <118808798+VincentP-EFREI@users.noreply.github.com>
2026-04-03 15:20:54 +0200

    Add files via upload

---
+++ vince/src/main/java/com/example/vince/VinceApplication.java
@@ -0,0 +1,13 @@
+package com.example.vince;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VinceApplication.class, args);
+	}
+
+}
---
+++ vince/src/test/java/com/example/vince/VinceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vince;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VinceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
